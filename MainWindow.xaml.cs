bbe3efe6d0dd94c843ecb1a7525068c1055cedef
denis <106389581+fishman123456@users.noreply.github.com>
2023-03-10 21:02:43 +0700

    дома разобраться

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -56,5 +57,16 @@ namespace _10_03_2023_Entity_Framework_forum
             }
 
         }
+
+        private void all_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable Author = new DataTable("Author");
+            DataColumn Author_id = new DataColumn("Id", Type.GetType("System.Int32"));
+            DataColumn Author_FName = new DataColumn("FirstName", Type.GetType("System.String"));
+            DataColumn Author_LName = new DataColumn("LastName", Type.GetType("System.String"));
+
+
+
+        } 
     }
 }
